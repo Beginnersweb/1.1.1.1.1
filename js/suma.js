e9979fa0665ad87fe73c7c1f3292e189911afc94
Beginners web <66438383+Beginnersweb@users.noreply.github.com>
2020-06-11 11:51:55 -0500

    Update suma.js

--- js/suma.js
+++ js/suma.js
@@ -4,14 +4,14 @@ var chi = Number(prompt("Cuantos chilaquiles piensa consumir (cuestan $75.00 pes
 if(chi <0){
     document.write("</p>Costo de los chilaquiles: $ 0 Pesos MXN")
 }else{
-document.write("</p>Costo de los chilaquiles: " + "$"+
+document.write("</p>Costo de los chilaquiles: $"+
  + 75*chi+"  Pesos MXN");
 }
 var hot = prompt("Cuantos HotCakes piensa consumir (cuestan $65.00 pesos MXN)");
 if(hot <0){
     document.write("</p>Costo de los chilaquiles: $ 0 Pesos MXN")
 }else{
-document.write("</p>Costo de los HotCakes: " + "$"+
+document.write("</p>Costo de los HotCakes: $"+
  + 65*hot+"  Pesos MXN");
 }
 
@@ -19,7 +19,7 @@ var mo = prompt("Cuantos Molletes piensa consumir (cuestan $45.00 pesos MXN)");
 if(mo <0){
     document.write("</p>Costo de los chilaquiles: $ 0 Pesos MXN")
 }else{
-document.write("</p>Costo de los Molletes: " + "$"+
+document.write("</p>Costo de los Molletes: $"+
  + 45*mo+"  Pesos MXN");
 }
 
@@ -28,21 +28,21 @@ var pe = prompt("Cuantos Pechuga piensa consumir (cuestan $75.00 pesos MXN)");
 if(pe <0){
     document.write("</p>Costo de los chilaquiles: $ 0 Pesos MXN")
 }else{
-document.write("</p>Costo de las Pechuga: " + "$"+
+document.write("</p>Costo de las Pechuga: $"+
  + 75*pe+"  Pesos MXN");
 }
 var mi = prompt("Cuantos Milanesa piensa consumir (cuestan $65.00 pesos MXN)");
 if(mi <0){
     document.write("</p>Costo de los chilaquiles: $ 0 Pesos MXN")
 }else{
-document.write("</p>Costo de las Milanesas: " + "$"+
+document.write("</p>Costo de las Milanesas:  $"+
  + 65*mi+"  Pesos MXN");
 }
 var po = prompt("Cuantos Pozole piensa consumir (cuestan $75.00 pesos MXN)");
 if(po <0){
     document.write("</p>Costo de los chilaquiles: $ 0 Pesos MXN")
 }else{
-document.write("</p>Costo de los Pozoles: " + "$"+
+document.write("</p>Costo de los Pozoles: $"+
  + 75*po+"  Pesos MXN");
 }
 res=0;
